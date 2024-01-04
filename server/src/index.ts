81f4bc0ada4217c4f48c1333d24c466441572708
totorobin <totorobin@gmail.com>
2024-01-04 13:37:57 +0100

    correction path rooms

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
-import express, { type Express } from 'express';
+import express, {type Express} from 'express';
 import dotenv from 'dotenv';
-import { createServer } from 'node:http';
-import { IoServer } from './ioServer';
+import {createServer} from 'node:http';
+import {IoServer} from './ioServer';
 import path from 'node:path';
 
 dotenv.config();
@@ -12,7 +12,7 @@ const server = createServer(app);
 
 app.use(express.static('public'));
 app.get('/room/:id', (_req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.resolve('public', 'index.html'));
 });
 
 const io = new IoServer(server);
